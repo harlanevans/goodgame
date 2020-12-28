b0bed05053c49f1e12f58f6df38ed2afd73f94f3
Adam Joesten <adamjoesten@gmail.com>
2020-12-28 13:22:55 -0700

    create preliminary Dashboard setup

--- client/components/login/Dashboard.jsx
+++ client/components/login/Dashboard.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-const Dashboard = () => {
-  return <h1>HELLO</h1>
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      desc: '',
+      genre: '',
+      platform: '',
+      review: '',
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>HI!</h1>
+        <Header />
+        <ResultsDisplay />
+      </div>
+    );
+  }
+}
+
+const Header = (props) => {
+  
+}
+
+const ResultsDisplay = (props) => {
+  
+}
+
+const result = (props) => {
+
 }
 
 export default Dashboard;
